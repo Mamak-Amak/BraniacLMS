475827303c95af6931d014bc5460568bb253826d
Mamak-Amak <105986370+Mamak-Amak@users.noreply.github.com>
2023-02-08 19:48:56 +0500

    Lesson 4 (#3)
    * lesson_3 URL and get params
    
    * lesson_4 Migrations
    
    * lesson_4 Fixtures
    
    * lesson_4 Data migration
    
    * lesson_4 Display model's data
    
    ---------
    
    Co-authored-by: mamak <mamak@mate.myguest.virtualbox.org>

--- mainapp/apps.py
+++ mainapp/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class MainappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mainapp"
-
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainapp'
