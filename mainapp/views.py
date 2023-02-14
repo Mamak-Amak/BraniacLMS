4e7246a3160d6a503c8f2d377432a78ce02ea7cd
Mamak-amak <sam.and.m@yandex.ru>
2023-02-14 20:58:26 +0500

    lesson_8 Tests

--- mainapp/views.py
+++ mainapp/views.py
@@ -68,6 +68,7 @@ class CoursesDetailView(TemplateView):
     template_name = "mainapp/courses_detail.html"
 
     def get_context_data(self, pk=None, **kwargs):
+        logger.debug("Yet another log message")
         context = super(CoursesDetailView, self).get_context_data(**kwargs)
         context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
         context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
@@ -88,6 +89,14 @@ class CoursesDetailView(TemplateView):
                 .select_related()
             )
             cache.set(f"feedback_list_{pk}", context["feedback_list"], timeout=300)  # 5 minutes
+
+            # Archive object for tests --->
+            #import pickle
+
+            #with open(f"mainapp/fixtures/005_feedback_list_{pk}.bin", "wb") as outf:
+            #   pickle.dump(context["feedback_list"], outf)
+            # <--- Archive object for tests
+
         else:
             context["feedback_list"] = cached_feedback
 
